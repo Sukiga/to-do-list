029db2566874d35fd50896fabd0b99ac5e99c137
Sukiga <maksiuheisuki@gmail.com>
2025-07-25 13:44:32 -0700

    fully implemented the add button gui

--- frontend/src/render.js
+++ frontend/src/render.js
@@ -7,3 +7,20 @@ var year = now.getFullYear();
 document.getElementById("day").innerHTML = `${day}`;
 document.getElementById("month").innerHTML = `${month}`;
 document.getElementById("year").innerHTML = `${year}`;
+
+// event handler for adding a task
+document.getElementById("addTask").onclick = function() {addTask()}
+
+// function for adding a task
+const listCon = document.getElementById("list-con");
+
+function addTask() {
+    let li = document.createElement("li");
+    li.classList.add("newTask");
+    li.innerHTML = "Add Task one two three four five six seven";
+    listCon.appendChild(li);
+    let deletes = document.createElement("span");
+    deletes.classList.add("deletes");
+    deletes.innerHTML = "\u00d7";
+    li.appendChild(deletes);
+}
