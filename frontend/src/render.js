d5a8836b117dc7da24de8dc96a673039cfff2546
Sukiga <maksiuheisuki@gmail.com>
2025-08-05 16:33:05 -0700

    implemented the checked tasks feature and delete feature

--- frontend/src/render.js
+++ frontend/src/render.js
@@ -13,16 +13,31 @@ document.getElementById("addTask").onclick = function() {addTask()}
 
 // function for adding a task
 const listCon = document.getElementById("list-con");
+let number = 0;
 
 function addTask() {
     let div = document.createElement("div");
-    div.classList.add("newTask");
-    div.classList.add("checked");
-    div.contentEditable = "true";
-    div.innerHTML = "Add";
+    div.classList.add("task");
+    div.id = generateId();
+    div.innerHTML = "<div id='input' contentEditable='true'></div>";
     listCon.appendChild(div);
     let deletes = document.createElement("span");
     deletes.classList.add("deletes");
     deletes.innerHTML = "\u00d7";
     div.appendChild(deletes);
 }
+
+function generateId() {
+    number ++;
+    return "task-" + number;
+}
+
+listCon.addEventListener("click", function(e) {
+    if (e.target.classList.contains("task")){
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
